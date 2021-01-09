45ecaf86d6dcb857dd7abaadfb02340865681987
Nadine <Nadine@DESKTOP-6E8NA7H>
2021-01-09 23:43:16 +0100

    Projektdateien hinzufügen.

---
+++ M120_Abschlussprojekt/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M120_Abschlussprojekt.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Spiele()
+        {
+            return View();
+        }
+
+        public ActionResult Start_Tetris()
+        {
+            return View();
+        }
+
+        public ActionResult Start_Memory()
+        {
+            return View();
+        }
+
+        public ActionResult Start_Snake()
+        {
+            return View();
+        }
+
+        public ActionResult Play_Tetris()
+        {
+            return View();
+        }
+
+        public ActionResult Play_Memory()
+        {
+            return View();
+        }
+        public ActionResult Play_Snake()
+        {
+            return View();
+        }
+    }
+}
---
+++ M120_Abschlussprojekt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="M120_Abschlussprojekt.MvcApplication" Language="C#" %>
